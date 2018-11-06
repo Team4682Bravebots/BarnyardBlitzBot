21cbfdd6c80d008e88126b413124ba86836cfeca
Akli Amrous <akamrous@gmail.com>
2018-11-05 19:32:10 -0800

    Add files via upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,66 +1,129 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/*
+
+______                    ______       _        ______                                      _  ______ _ _ _       
+| ___ \                   | ___ \     | |       | ___ \                                    | | | ___ \ (_) |      
+| |_/ /_ __ __ ___   _____| |_/ / ___ | |_ ___  | |_/ / __ _ _ __ _ __  _   _  __ _ _ __ __| | | |_/ / |_| |_ ____
+| ___ \ '__/ _` \ \ / / _ \ ___ \/ _ \| __/ __| | ___ \/ _` | '__| '_ \| | | |/ _` | '__/ _` | | ___ \ | | __|_  /
+| |_/ / | | (_| |\ V /  __/ |_/ / (_) | |_\__ \ | |_/ / (_| | |  | | | | |_| | (_| | | | (_| | | |_/ / | | |_ / / 
+\____/|_|  \__,_| \_/ \___\____/ \___/ \__|___/ \____/ \__,_|_|  |_| |_|\__, |\__,_|_|  \__,_| \____/|_|_|\__/___|
+                                                                         __/ |                                    
+                                                                        |___/                            
+*/
+
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import jaci.pathfinder.*;
+import edu.wpi.first.wpilibj.PIDController;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SerialPort;
+import com.kauailabs.navx.frc.AHRS.SerialDataType;
 
 public class Robot extends IterativeRobot {
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+//Define the Joysticks
+Joystick JoyL = new Joystick(0); //Left Joystick
+Joystick JoyR = new Joystick(1); //Right Joystick
 
-Joystick ljoy = new Joystick(0);
-Joystick rjoy = new Joystick(1);
-double  joyLY = 0;
-double joyRY = 0;
+//Define joystick speeds
+double JoyLY;
+double JoyRY;
+
+//Define the Motors
+TalonSRX fl = new TalonSRX(0); //Front left
+TalonSRX fr = new TalonSRX(1); //Front Right
+TalonSRX bl = new TalonSRX(2); //Back Left
+TalonSRX br = new TalonSRX(3); // Back Right
 
 
-  @Override
-  public void robotInit() {
+
+
+//Define the Gyro
+AHRS ahrs = new AHRS(SerialPort.Port.kMXP, SerialDataType.kProcessedData, (byte)50);
+
+
+
+//tankDrive() >>>>>>>>>>>>>>>> dt()
+
+///@param fls front left motor speed
+///@param frs front right motor speed
+///@param bls back left motor speed
+///@param brs back right motor speed
+  public void tankDrive(double FLS, double FRS, double BLS, double BRS)
+  {
+    
+    fl.set(ControlMode.PercentOutput, FLS);
+    fr.set(ControlMode.PercentOutput, FRS);
+    bl.set(ControlMode.PercentOutput, BLS);
+    br.set(ControlMode.PercentOutput, BRS);
+
+
+  }
+
+///@param fls front left motor speed
+///@param frs front right motor speed
+///@param bls back left motor speed
+///@param brs back right motor speed
+//Autonomous drive command
+  public void autoDrive(double FLS, double FRS, double BLS, double BRS)
+  {
    
   }
 
+@Override
+  public void robotInit() 
+  {
+   
+
+  }
+
 
   @Override
-  public void robotPeriodic() {
+  public void robotPeriodic() 
+  {
+
   }
 
 
   @Override
-  public void autonomousInit() {
+  public void autonomousInit() 
+  {
 
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
-  public void autonomousPeriodic() {
+  public void autonomousPeriodic()
+  {
 
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
-  public void teleopPeriodic() {
+  public void teleopPeriodic() 
+  {
+  
+
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
-  public void testPeriodic() {
+  public void testPeriodic() 
+  {
   }
 }
